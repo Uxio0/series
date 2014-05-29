cde392068bda384ac54f0659a622ba6c0e74e8f1
Uxio <uxio11@gmail.com>
2014-05-29 15:11:26 +0200

    Refractored renamer

--- renamer.py
+++ renamer.py
@@ -5,35 +5,43 @@ import re
 import sys
 import os
 import os.path
-from subprocess import call
+from os.path import join
 import glob
+
+shows_folder = "/mnt/TOURO/shows/"
+films_folder = "/mnt/TOURO/peliculas/"
 
 
 def crea_enlaces_simbolicos(archivo):
-	nombre_archivo = os.path.basename(archivo)
-	try:
-	    serie, temporada, _ = re.findall("(.*?)S(\d{2})E(\d{2})", nombre_archivo.replace(".", " "))[0]
-	    serie, temporada = serie.strip(), temporada.strip()
-	    call(["mkdir", "-p", "/mnt/TOURO/shows/{}/Season {}/".format(serie, temporada)])
-	    destino = "/mnt/TOURO/shows/{}/Season {}/{}".format(serie, temporada,
-							    nombre_archivo)
-	except:
-	    destino = "/mnt/TOURO/peliculas/{}".format(nombre_archivo)
-	    
-	if not call(["ln", "-s", os.path.abspath(archivo),
-		       destino]):
-	    print "{}".format(destino)
+    nombre_archivo = os.path.basename(archivo)
+    try:
+        serie, temporada, _ = re.findall("(.*?)S(\d{2})E(\d{2})",
+                                         nombre_archivo.replace(".", " "))[0]
+        serie, temporada = serie.strip(), temporada.strip()
+        os.mkdir(join(shows_folder, "{}/Season {}/").format(serie, temporada))
+        target = join(shows_folder, "{}/Season {}/{}").format(serie, temporada,
+                                                               nombre_archivo)
+    except OSError:
+        target = None
+
+    except TypeError:
+        #Regular expression fail
+        target = join(films_folder, "{}").format(nombre_archivo)
+
+    if target:
+        os.symlink(os.path.abspath(archivo), target)
+        print("{}".format(target))
+
 
 if len(sys.argv) == 2:
-	archivo = sys.argv[1]
-	if os.path.isdir(archivo):
-		os.chdir(archivo)
-		tipos = ("*.mkv", "*.mp4")
-		archivos = [y for x in tipos for y in glob.glob(x)]
-		for archivo in archivos:
-			crea_enlaces_simbolicos(archivo)
-	else:
-		crea_enlaces_simbolicos(archivo)
+    archivo = sys.argv[1]
+    if os.path.isdir(archivo):
+        os.chdir(archivo)
+        tipos = ("*.mkv", "*.mp4")
+        archivos = [y for x in tipos for y in glob.glob(x)]
+        for archivo in archivos:
+            crea_enlaces_simbolicos(archivo)
+    else:
+        crea_enlaces_simbolicos(archivo)
 else:
-	sys.exit(1)
-
+    sys.exit(1)

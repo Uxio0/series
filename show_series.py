5fbbad73056a5a358ec9e88e07dac699a2e59dda
Uxio <uxio11@gmail.com>
2014-05-29 15:23:51 +0200

    Showing last date

--- show_series.py
+++ show_series.py
@@ -3,13 +3,18 @@
 
 from codecs import open
 import json
+import tvdb_api
 
 with open('series.json') as f:
     nombre_series = json.loads(f.read())
 with open('vistas.json') as f:
     vistas = json.loads(f.read())
 
+tv = tvdb_api.Tvdb()
 for k, v in vistas.iteritems():
-    print("{} -> Season {} Episode {}".format(nombre_series[k],
-                                              v['season'],
-                                              v['episode']))
+    show = tv[nombre_series[k]]
+    aired = show[v['season']][v['episode']]['firstaired']
+    print("{} -> Season {} Episode {} Date {}".format(nombre_series[k],
+                                                      v['season'],
+                                                      v['episode'],
+                                                      aired))

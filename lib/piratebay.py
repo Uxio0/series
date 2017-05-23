7365db528475343c022ac94d013cabe60ef9b077
Uxío <Uxio0@users.noreply.github.com>
2017-05-23 20:48:52 +0200

    Update piratebay proxy

--- lib/piratebay.py
+++ lib/piratebay.py
@@ -2,7 +2,7 @@ from searchEngine import SearchEngine
 
 
 class PirateBay(SearchEngine):
-    search_url = u'https://pirateproxy.vip/search/{}/0/7/0'
+    search_url = u'https://pirateproxy.cc/search/{}/0/7/0'
 
 
 if __name__ == "__main__":
